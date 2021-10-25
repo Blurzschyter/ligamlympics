1bbdddcf79c4a6cc54509c26ca0cd7c48363e45f
Sharulnizar <sharulnizar.razali@gmail.com>
2021-10-25 11:32:26 +0800

    debug log enable

--- cc/js/receiver.js
+++ cc/js/receiver.js
@@ -1,4 +1,20 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
+// Debug Logger
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+const LOG_TAG = 'MyAPP.LOG';
+
+// Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+castDebugLogger.setEnabled(true);
+
+// Show debug overlay
+castDebugLogger.showDebugLogs(true);
+
+// Set verbosity level for Core events.
+castDebugLogger.loggerLevelByEvents = {
+  'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
+  'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
+}
+
 context.start();
